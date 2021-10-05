01f1392130c90257f0f259c885854499052f8157
JingleJenks <sylv2439@stud.kea.dk>
2021-10-05 13:36:29 +0200

    freediver animation ggoes on scroll

--- splash.js
+++ splash.js
@@ -1,20 +1,38 @@
-window.addEventListener("load", loadWelcome);
+// window.addEventListener("load", loadWelcome);
 
 
-function loadWelcome() {
-    console.log("loadWelcome");
-    document.querySelector(".text").addEventListener("click", loadOcean);
-}
+// function loadWelcome() {
+//     console.log("loadWelcome");
+//     document.querySelector(".text").addEventListener("click", loadOcean);
+// }
 
-function loadOcean() {
-    console.log("loadOcean");
-    document.querySelector(".container").addEventListener("click", Ocean);
-}
+// function loadOcean() {
+//     console.log("loadOcean");
+//     document.querySelector(".container").addEventListener("click", Ocean);
+// }
 
-function Ocean() {
-    console.log("scrollOcean");
-    document.querySelector("#screen_ocean").classList.remove("hidden");
-    document.querySelector("#screen_welcome").classList.remove("welcome_grid");
+// function Ocean() {
+//     console.log("scrollOcean");
+//     document.querySelector("#screen_ocean").classList.remove("hidden");
+//     document.querySelector("#screen_welcome").classList.remove("welcome_grid");
 
-    document.querySelector("#screen_welcome").classList.add("hidden");
-}
+//     document.querySelector("#screen_welcome").classList.add("hidden");
+// }
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    console.log("1");
+    entries.forEach(entry => {
+        // If the element is visible
+        console.log("2");
+
+        if (entry.isIntersecting) {
+            // Add the animation class
+            entry.target.classList.add('ani_freediver');
+        }
+    });
+    console.log("3");
+
+});
+
+observer.observe(document.querySelector('.freediver'));
