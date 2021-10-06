32ead505bbcb2adb20645480b477b679fc155235
JingleJenks <sylv2439@stud.kea.dk>
2021-10-06 12:05:31 +0200

    js added

--- splash.js
+++ splash.js
@@ -18,7 +18,6 @@ const observer40 = new IntersectionObserver(entries => {
 });
 observer40.observe(document.querySelector('.diver-wrapper'));
 
-
 //FREEDIVER SCROLL ANIMATION 
 const observer120 = new IntersectionObserver(entries => {
     // Loop over the entries
@@ -38,3 +37,24 @@ const observer120 = new IntersectionObserver(entries => {
 });
 
 observer120.observe(document.querySelector('.freediver-wrapper'));
+
+
+//SPERM SCROLL ANIMATION 
+const observer1000 = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+
+        const sperm = entry.target.querySelector('.sperm');
+
+        if (entry.isIntersecting) {
+            // Add the animation class
+            sperm.classList.add('ani_sperm');
+            return; //if we added the class, end of function 
+        }
+        //once we are no more interesecting, remove the class so that is ready to be added again when in viewport again 
+        sperm.classList.remove('ani_sperm')
+    });
+
+});
+
+observer1000.observe(document.querySelector('.sperm-wrapper'));
