b271d1022ba8b05f6d85dc8381febff92eb83913
JingleJenks <sylv2439@stud.kea.dk>
2021-10-06 11:25:33 +0200

    animation and js created

--- splash.js
+++ splash.js
@@ -1,40 +1,66 @@
+// //DIVER SCROLL ANIMATION 
+// const observer = new IntersectionObserver(entries => {
+//     // Loop over the entries
+//     entries.forEach(entry => {
+//         const diver = entry.target.querySelector('.diver');
+
+//         if (entry.isIntersecting) {
+//             // Add the animation class
+//             freediver.classList.add('ani_diver');
+//             return; //if we added the class, end of function 
+//         }
+//         //once we are no more interesecting, remove the class so that is ready to be added again when in viewport again 
+//         freediver.classList.remove('ani_diver')
+//     });
+
+// });
+
+// observer.observe(document.querySelector('.diver-wrapper'));
+
+
+
 //FREEDIVER SCROLL ANIMATION 
 const observer = new IntersectionObserver(entries => {
     // Loop over the entries
     entries.forEach(entry => {
-        const freediver = entry.target.querySelector('.freediver');
+        // const freediver = entry.target.querySelector('.freediver');
+        const diver = entry.target.querySelector('.diver');
 
         if (entry.isIntersecting) {
             // Add the animation class
-            freediver.classList.add('ani_freediver');
+            // freediver.classList.add('ani_freediver');
+            diver.classList.add('ani_diver');
+
             return; //if we added the class, end of function 
         }
         //once we are no more interesecting, remove the class so that is ready to be added again when in viewport again 
-        freediver.classList.remove('ani_freediver')
+        // freediver.classList.remove('ani_freediver')
+        diver.classList.remove('ani_diver')
+
     });
 
 });
 
 observer.observe(document.querySelector('.freediver-wrapper'));
+observer.observe(document.querySelector('.diver-wrapper'));
 
-//DIVER SCROLL ANIMATION 
 
-// window.addEventListener("load", loadWelcome);
 
-// function loadWelcome() {
-//     console.log("loadWelcome");
-//     document.querySelector(".text").addEventListener("click", loadOcean);
-// }
+// const observer = new IntersectionObserver(entries => {
+//     // Loop over the entries
+//     entries.forEach(entry => {
 
-// function loadOcean() {
-//     console.log("loadOcean");
-//     document.querySelector(".container").addEventListener("click", Ocean);
-// }
+//         const freediver = entry.target.querySelector('.freediver');
 
-// function Ocean() {
-//     console.log("scrollOcean");
-//     document.querySelector("#screen_ocean").classList.remove("hidden");
-//     document.querySelector("#screen_welcome").classList.remove("welcome_grid");
+//         if (entry.isIntersecting) {
+//             // Add the animation class
+//             freediver.classList.add('ani_freediver');
+//             return; //if we added the class, end of function 
+//         }
+//         //once we are no more interesecting, remove the class so that is ready to be added again when in viewport again 
+//         freediver.classList.remove('ani_freediver')
+//     });
 
-//     document.querySelector("#screen_welcome").classList.add("hidden");
-// }
+// });
+
+// observer.observe(document.querySelector('.freediver-wrapper'));
