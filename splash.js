a321755ffa4b1eed192f5e5882ddd5b3e136b1d3
JingleJenks <sylv2439@stud.kea.dk>
2021-10-06 11:31:34 +0200

    freediver&diver combined

--- splash.js
+++ splash.js
@@ -1,66 +1,40 @@
 // //DIVER SCROLL ANIMATION 
-// const observer = new IntersectionObserver(entries => {
-//     // Loop over the entries
-//     entries.forEach(entry => {
-//         const diver = entry.target.querySelector('.diver');
-
-//         if (entry.isIntersecting) {
-//             // Add the animation class
-//             freediver.classList.add('ani_diver');
-//             return; //if we added the class, end of function 
-//         }
-//         //once we are no more interesecting, remove the class so that is ready to be added again when in viewport again 
-//         freediver.classList.remove('ani_diver')
-//     });
-
-// });
-
-// observer.observe(document.querySelector('.diver-wrapper'));
-
-
-
-//FREEDIVER SCROLL ANIMATION 
-const observer = new IntersectionObserver(entries => {
+const observer40 = new IntersectionObserver(entries => {
     // Loop over the entries
     entries.forEach(entry => {
-        // const freediver = entry.target.querySelector('.freediver');
         const diver = entry.target.querySelector('.diver');
 
         if (entry.isIntersecting) {
             // Add the animation class
-            // freediver.classList.add('ani_freediver');
-            diver.classList.add('ani_diver');
+            diver.classList.add('ani_diver')
 
             return; //if we added the class, end of function 
         }
         //once we are no more interesecting, remove the class so that is ready to be added again when in viewport again 
-        // freediver.classList.remove('ani_freediver')
         diver.classList.remove('ani_diver')
 
     });
 
 });
-
-observer.observe(document.querySelector('.freediver-wrapper'));
-observer.observe(document.querySelector('.diver-wrapper'));
+observer40.observe(document.querySelector('.diver-wrapper'));
 
 
+//FREEDIVER SCROLL ANIMATION 
+const observer120 = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
 
-// const observer = new IntersectionObserver(entries => {
-//     // Loop over the entries
-//     entries.forEach(entry => {
+        const freediver = entry.target.querySelector('.freediver');
 
-//         const freediver = entry.target.querySelector('.freediver');
+        if (entry.isIntersecting) {
+            // Add the animation class
+            freediver.classList.add('ani_freediver');
+            return; //if we added the class, end of function 
+        }
+        //once we are no more interesecting, remove the class so that is ready to be added again when in viewport again 
+        freediver.classList.remove('ani_freediver')
+    });
 
-//         if (entry.isIntersecting) {
-//             // Add the animation class
-//             freediver.classList.add('ani_freediver');
-//             return; //if we added the class, end of function 
-//         }
-//         //once we are no more interesecting, remove the class so that is ready to be added again when in viewport again 
-//         freediver.classList.remove('ani_freediver')
-//     });
+});
 
-// });
-
-// observer.observe(document.querySelector('.freediver-wrapper'));
+observer120.observe(document.querySelector('.freediver-wrapper'));
